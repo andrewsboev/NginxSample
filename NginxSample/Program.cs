097bf78f2006031c4a8d81b60d0af42897b94c70
sboev <sboev@kontur.ru>
2020-04-09 16:46:21 +0500

    First

---
+++ NginxSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace NginxSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prefix = args.First();
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add(prefix);
+            httpListener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Listening");
+                var context = httpListener.GetContext();
+                var request = context.Request;
+                var response = context.Response;
+                var responseString = $"<HTML><BODY> Hello from {prefix} </BODY></HTML>";
+                var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                var output = response.OutputStream;
+                output.Write(buffer,0,buffer.Length);
+                output.Close();
+            }
+        }
+    }
+}
